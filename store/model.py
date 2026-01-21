6455b58f1b185d5ef8548d4bd8aaf5bdd2c1e08d
abasshodrog9-droid <abass.hodrog9@gmail.com>
2026-01-21 23:52:03 +0200

    Update model.py

--- store/model.py
+++ store/model.py
@@ -1,10 +1,13 @@
+
+
 from dataclasses import dataclass
 from typing import List, Dict
+
 @dataclass
 class Note:
-    title: str
-    tags: List[str]
-    cipher: str                   
-    params: Dict[str, str | int]   
-    ciphertext_b64: str            
-    created_at: str                
+    title: str                 
+    tags: List[str]            
+    cipher: str                
+    params: Dict[str, str]     
+    ciphertext_b64: str       
+    created_at: str           
